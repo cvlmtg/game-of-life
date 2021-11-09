9d83a7258b21ad44d0e3f38ccb6dd7259387acd6
Matteo Cavalleri <cvlmtg@gmail.com>
2021-11-09 21:54:05 +0100

    fill the grid when setting the size

--- src/reducers/game-reducer.ts
+++ src/reducers/game-reducer.ts
@@ -1,11 +1,26 @@
 import { State } from '../records/game-records';
 import * as actions from '../constants/actions';
+import { DEAD } from '../constants/constants';
+import type { Grid } from '../utils/board';
 import type { Action } from 'monarc';
 
 // --------------------------------------------------------------------
 
 function updateGridSize(state: State, action: Action): State {
-  return state.set('size', action.size);
+  const size: number = action.size;
+  const grid: Grid   = [];
+
+  for (let i = 0; i < size; i++) {
+    const empty = new Array(size);
+    const row   = empty.fill(DEAD);
+
+    grid.push(row);
+  }
+
+  return state.withMutations((mutable) => {
+    mutable.set('grid', grid);
+    mutable.set('size', size);
+  });
 }
 
 // --------------------------------------------------------------------

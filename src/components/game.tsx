2b13063bbb1635f7345cf3869a67db5802ce12c2
Matteo Cavalleri <cvlmtg@gmail.com>
2021-11-11 18:51:36 +0100

    move useSimulation in the hooks file
    even if it's used just in one place, make
    the component source code a little tidier

--- src/components/game.tsx
+++ src/components/game.tsx
@@ -1,58 +1,13 @@
+import { FunctionComponent, Fragment } from 'react';
 import Buttons from './game/buttons';
 import Presets from './game/presets';
 import Wrapper from './game/wrapper';
-import {
-  FunctionComponent, Fragment, useCallback, useEffect, useState
-} from 'react';
 
-import { nextGeneration } from '../actions/game-actions';
 import type { State } from '../records/game-records';
+import { useSimulation } from '../utils/hooks';
 import { hasCells } from '../utils/board';
-import { useDispatch } from 'monarc';
 
 // --------------------------------------------------------------------
-
-function useSimulation(empty: boolean): [ boolean, OnToggle ] {
-  const [ running, setRunning ] = useState(false);
-
-  const dispatch = useDispatch();
-
-  const onInterval = useCallback(() => {
-    nextGeneration(dispatch);
-  }, [ dispatch ]);
-
-  useEffect(() => {
-    let timer: ReturnType<typeof setTimeout>;
-
-    if (empty) {
-      setRunning(false);
-      return undefined;
-    }
-
-    // XXX un'animazione troppo veloce rende l'esperienza
-    // abbastanza confusa. introduciamo quindi un ritardo
-    // artificiale (con un sistema abbastanza crudo, ma
-    // per il momento ci accontentiamo)
-
-    if (running) {
-      timer = setInterval(onInterval, 250);
-
-      return () => {
-        clearInterval(timer);
-      };
-    }
-
-    return undefined;
-  }, [ empty, running, onInterval ]);
-
-  if (running === true) {
-    return [ true, () => setRunning(false) ];
-  }
-
-  return [ false, () => setRunning(true) ];
-}
-
-type OnToggle = () => void;
 
 interface Props {
   store: State;
--- src/utils/hooks.ts
+++ src/utils/hooks.ts
@@ -1,7 +1,53 @@
-import { useMemo } from 'react';
+import { useCallback, useEffect, useState, useMemo } from 'react';
+import { nextGeneration } from '../actions/game-actions';
+import { useDispatch } from 'monarc';
+
+// --------------------------------------------------------------------
+
+type OnToggle = () => void;
 
 // --------------------------------------------------------------------
 
 export function useRandomId(): string {
   return useMemo(() => Math.random().toString(36).substring(2, 6) || '-', []);
+}
+
+export function useSimulation(empty: boolean): [ boolean, OnToggle ] {
+  const [ running, setRunning ] = useState(false);
+
+  const dispatch = useDispatch();
+
+  const onInterval = useCallback(() => {
+    nextGeneration(dispatch);
+  }, [ dispatch ]);
+
+  useEffect(() => {
+    let timer: ReturnType<typeof setTimeout>;
+
+    if (empty) {
+      setRunning(false);
+      return undefined;
+    }
+
+    // XXX un'animazione troppo veloce rende l'esperienza
+    // abbastanza confusa. introduciamo quindi un ritardo
+    // artificiale (con un sistema abbastanza crudo, ma
+    // per il momento ci accontentiamo)
+
+    if (running) {
+      timer = setInterval(onInterval, 250);
+
+      return () => {
+        clearInterval(timer);
+      };
+    }
+
+    return undefined;
+  }, [ empty, running, onInterval ]);
+
+  if (running === true) {
+    return [ true, () => setRunning(false) ];
+  }
+
+  return [ false, () => setRunning(true) ];
 }
